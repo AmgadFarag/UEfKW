675944ce42c3cd8377c9f19583fe8163fd74ef23
ObsoleteAmigo <amagido2@gmail.com>
2018-12-03 22:21:54 +0200

    Normal Level Initial Layout

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+	public GameObject target;
+	private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+		if (target == null)
+			target = GameObject.FindGameObjectWithTag ("Player");
+		offset = transform.position - target.transform.position;
+		//offset.position = target.GetComponent<Transform> ().position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = target.GetComponent<Transform> ().position + offset;
+	}
+}
